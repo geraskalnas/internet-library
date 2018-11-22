baa8634c40146270f638ddfeecd3f8726d072ec4
geraskalnas <geraskalnas@gmail.com>
2018-11-22 14:07:02 +0200

    Added nav

--- index.php
+++ index.php
@@ -133,6 +133,12 @@ echo "";
   <link rel="stylesheet" href="css/style.css">  
 </head>
 <body>
+  <div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
   <div class="wrapper">
    
     
