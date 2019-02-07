283621611de4b06a29653fa737cd7f973c170e1d
geraskalnas <geraskalnas@gmail.com>
2019-02-07 22:11:32 +0200

    show.php

--- show.php
+++ show.php
@@ -1,7 +1,7 @@
 <?php
 require_once("classes.php");
 require_once("config.php");
-include("templateEngine.php");
+require_once("pre.php");
 
 /*
 Pagrindas
@@ -12,37 +12,14 @@ Pagrindas
 */
 
 //Pagrindas
-$layout = new Template("templates/layout.t");
-$layout->set("title", $PAGE_TITLE);
-
-	$layout->set("username", $name);
-
 	//"ivynioklis"
-	$wrapper = new Template("templates/wrapper.t");
-		//Sonine juosta
-		$sidebar = new Template("templates/sidebar.t");
-		$sidebar->set("title", "Paskutines 10:");
-			$sql = "SELECT id, name, author FROM books ORDER BY id desc;";
-			if (!$result = $db->query($sql)) {
-				die('There was an error running the query [' . $db->error . ']');
-			}
-			while ($row = $result->fetch_assoc()){
-				$book = new Template("templates/sidebarBookLink.t");
-				$book->set("name", $row["author"]." „".$row["name"]."“");
-				$book->set("url", "show.php?id=".$row["id"]);
-				$booksTemplates[]=$book;
-			}
-			$books = Template::merge($booksTemplates);
-			$sidebar->set("content", $books);
-			$wrapper->set("sidebar", $sidebar->output());
-			unset ($booksTemplates);
 			//Knygos
 			$bookO->loadById($_GET["id"]);
             $book=new Template("templates/show_book.t");
 				$book->set("name", $bookO->get_name());
 				$book->set("author", $bookO->get_author());
 				$book->set("imgUrl", $bookO->get_imgPath());
-        $wrapper->set("books", $book->output());
-		$layout->set("content", $wrapper->output());
+				$book->set("pdfUrl", $bookO->get_pdfPath());
+            $layout->set("content", $book->output());
 echo $layout->output();
 ?>
