c626fe433170ffd5b8b5b5fb3be25c9c92d60cf5
geraskalnas <geraskalnas@gmail.com>
2019-04-24 18:32:19 +0300

    Removed comment, newlines, enabled action variable

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php
- 
     require_once("resources/config.php");
- 
     require_once(LIBRARY_PATH . "/templateFunctions.php");
- 
     
-    $sep=isset($_GET["size"])?$_GET["size"]:5;
-	$p=isset($_GET["page"])?$_GET["page"]-1:0;
-	if($p<0) $p=0;
-	if($sep<2) $sep=5;
-	
-     
-    // Must pass in variables (as an array) to use in template
+    $action=isset($_GET["action"])?$_GET["action"]:"home";
+    
     $variables = array(
-        'sep' => $sep,
-        'p' => $p,
         'db' => $db,
         'username' => USERNAME
     );
-     
-    renderLayoutWithContentFile("home.php", $variables);
- 
+    
+    switch($action){
+    	case "home":
+    		$sep=isset($_GET["size"])?$_GET["size"]:5;
+			$p=isset($_GET["page"])?$_GET["page"]-1:0;
+			if($p<0) $p=0;
+			if($sep<2) $sep=5;
+			$variables["p"]=$p;
+			$variables["sep"]=$sep;
+    		break;
+    	case "database":
+    		break;
+    	default:
+    		$action = "error";
+    }
+    
+    $variables["title"] = "LIBEPIC - ".$action;
+    
+    renderLayoutWithContentFile($action.".php", $variables);
 ?>
